d3573cf56bb3202544dbd1ff9a3e27d81604af75
Anshika Chaudhary <anshikachaudhary981@gmail.com>
2020-06-13 21:32:24 +0530

    Updates

---
+++ precache-manifest.6d00741a2ab2c832f6defafee6b4b721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8be14645d933c1f56ce8b30d5b89096",
+    "url": "/anshikachaudhary981/React-To-Do/index.html"
+  },
+  {
+    "revision": "1df112403d26cae89121",
+    "url": "/anshikachaudhary981/React-To-Do/static/css/main.37ce2fc3.chunk.css"
+  },
+  {
+    "revision": "d8e0b5cb1c9c6c48e788",
+    "url": "/anshikachaudhary981/React-To-Do/static/js/2.fb5eb7ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anshikachaudhary981/React-To-Do/static/js/2.fb5eb7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df112403d26cae89121",
+    "url": "/anshikachaudhary981/React-To-Do/static/js/main.bc79dc36.chunk.js"
+  },
+  {
+    "revision": "495d3890a590c0049f6a",
+    "url": "/anshikachaudhary981/React-To-Do/static/js/runtime-main.9d5e800d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anshikachaudhary981/React-To-Do/static/media/logo.5d5d9eef.svg"
+  }
+]);
